9b8d05603b7d71382a055285764918512470afc1
Arthur Duarte <zarthurbr2@gmail.com>
2023-05-31 15:51:44 -0300

    teste sobre array

---
+++ Teste_array.js
@@ -0,0 +1,9 @@
+let nomes = ["Glaucio", "Sirlaine", "Arthur"]
+
+let idades = [52, 43, 22]
+
+let cargos = ["Pai", "Mae", "Filho"]
+
+let arraymaster= [nomes, idades, cargos]
+
+console.log(`${arraymaster[0][2]} tem ${arraymaster[1][2]} anos de idade e e filho de ${arraymaster[0][0]} e ${arraymaster[0][1]}`)
