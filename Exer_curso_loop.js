23979e3ec02429c84128c599b743349037149903
Arthur Duarte <zarthurbr2@gmail.com>
2023-06-16 15:52:21 -0300

    Teste e exercicios sobre loop

---
+++ Exer_curso_loop.js
@@ -0,0 +1,17 @@
+// 1. Faça um loop que mostre todas as frutas
+let frutas = ['banana', 'uva', 'morango']
+
+// 2. Conte até 100 usando um WHILE
+
+// 1.
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}
+
+// 2.
+let count = 1
+
+while (count <= 100) {
+    console.log(count)
+    count++
+}
---
+++ Teste_while.js
@@ -0,0 +1,6 @@
+let x = 0
+
+while (x < 10) {
+    console.log(`O valor dessa vez é ${x}`)
+    x++
+}
