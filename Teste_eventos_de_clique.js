cae570d5f04dca10b6b34c628eebf5d1d1cf8e35
Arthur Duarte <zarthurbr2@gmail.com>
2023-06-17 15:34:19 -0300

    Testes com eventos de clique (pelo arquivo .js e .html)

---
+++ Teste_eventos_de_clique.js
@@ -0,0 +1,23 @@
+
+function clique() {
+    console.log("Clicou no botão")
+}
+/*
+let x = document.querySelector('.botao1')
+x.addEventListener("click", clique)
+
+/*
+VARIAÇÕES
+
+let x = document.querySelector('.botao1')
+x.addEventListener("click", function (){
+    clique()
+})
+
+OU
+
+let x = document.querySelector('.botao1')
+x.addEventListener("click", () => {
+    clique()
+})
+*/
